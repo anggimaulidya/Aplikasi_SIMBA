a9cddc6e8b1ccca21cad8e9d9b1b69f695f45a8f
Nadiavicki <vickihamidah@gmail.com>
2024-12-02 13:33:54 +0700

    Edit tanggal masuk keluar data pasien

--- src/SIMBA/EditDataPasien.java
+++ src/SIMBA/EditDataPasien.java
@@ -269,9 +269,8 @@ public class EditDataPasien extends javax.swing.JFrame {
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addContainerGap()
                 .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 359, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
